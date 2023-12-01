6f89fa00dafca73d76c8d61dbbd2b1d5b3c75cca
mixueicecreamandtea <qwertyuiop9296@outlook.com>
2023-12-01 22:33:41 +0800

    士大夫十分

--- static/index.js
+++ static/index.js
@@ -278,35 +278,14 @@ const MODE_NORMAL = 1, MODE_ENDLESS = 2, MODE_PRACTICE = 3;
     }
 
     function SubmitResults() {
-        let system = "其他操作系统";
-        let area = "异世界";
         if ($("#username").val() && _gameSettingNum === 20) {
-            const systems = [
-                ['Win', 'Windows'],
-                ['like Mac', 'iOS'],
-                ['Mac', 'Macintosh'],
-                ['Android', 'Android'],
-                ['Linux', 'Linux'],
-            ];
-
-            for (let sys of systems) {
-                if (navigator.appVersion.indexOf(sys[0]) !== -1) {
-                    system = sys[1];
-                    break;
-                }
-            }
-
-            if (returnCitySN && returnCitySN['cname']) {
-                area = returnCitySN['cname']
-            }
-
             let httpRequest = new XMLHttpRequest();
             httpRequest.open('POST', './SubmitResults.php', true);
             httpRequest.setRequestHeader("Content-type", "application/json");
             let name = $("#username").val();
             let message = $("#message").val();
             let test = "|_|";
-            httpRequest.send(encrypt(_gameScore + test + name + test + tj + test + system + test + area + test + message));
+            httpRequest.send(encrypt(_gameScore + test + name + test + tj + test + message));
         }
     }
 
@@ -645,31 +624,21 @@ const MODE_NORMAL = 1, MODE_ENDLESS = 2, MODE_PRACTICE = 3;
                    background-size: auto 100%;
                    background-image: url(${r});
             }`);
-		})
-	}
+        })
+    }
 
-	w.getClickAfterImage = function()
-	{
-		$('#click-after-image')
-			.click();
-	}
+    w.getClickAfterImage = function() {
+        $('#click-after-image').click();
+    }
 
-	w.saveClickAfterImage = function()
-	{
-		const img = document.getElementById('click-after-image');
-		saveImage(img, r =>
-		{
-			clickAfterStyle.html(`
+    w.saveClickAfterImage = function() {
+        const img = document.getElementById('click-after-image');
+        saveImage(img, r => {
+            clickAfterStyle.html(`
                 .tt1, .tt2, .tt3, .tt4, .tt5 {
                   background-size: auto 86%;
                   background-image: url(${r});
             }`);
-<<<<<<< HEAD
-		})
-	}
-})(window);
-=======
         })
     }
 }) (window);
->>>>>>> parent of ada75ea (Update index.js)
